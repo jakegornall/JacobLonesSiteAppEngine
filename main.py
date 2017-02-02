d03d43375f108320d379493576d8fd177ca0d757
jakegornall <jakegornall@yahoo.com>
2017-02-02 12:27:46 -0500

    Add localhost debugging
    chore: Add code for localhost debugging

--- main.py
+++ main.py
@@ -13,3 +13,7 @@ def server_error(e):
     # Log the error and stacktrace.
     logging.exception('An error occurred during a request.')
     return'An internal error occurred.', 500
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host="127.0.0.1", port=5000)
