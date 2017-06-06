ef93a47db29b86f3c5562346040417eb749e4184
Jake Gornall <jakegornall@yahoo.com>
2017-06-06 01:00:31 -0400

    feat: add admin login functionality
    admin can now log in at /admin. Once authorized, a cookie is set and the admin can edit the page directly.

--- main.py
+++ main.py
@@ -1,11 +1,41 @@
 import logging
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, make_response, session, redirect
+import random
 
 app = Flask(__name__)
 
+def isLoggedIn():
+    loggedIn = False
+    access = request.cookies.get('access')
+    if str(access) == str(session["access"]):
+        loggedIn = True
+    return loggedIn
+
+
 @app.route('/')
 def main():
-    return render_template("LandingPage.html")
+    if isLoggedIn():
+        return render_template("LandingPage.html", loggedIn=True)    
+    else:
+        return render_template("LandingPage.html", loggedIn=False)
+
+@app.route('/admin', methods=['GET', 'POST'])
+def admin():
+    if request.method == 'GET':
+        return render_template("adminLogin.html")
+
+    if request.method == 'POST':
+        # if valid credentials,
+        # set cookie and redirect to main()
+        
+        if request.form["email"] == 'jakegornall@yahoo.com' and request.form['password'] == 'password':
+            session["access"] = random.randint(00000, 99999)
+            response = make_response(redirect('/'))
+            response.set_cookie('access', str(session["access"]))
+            return response
+
+        else:
+            return render_template("adminLogin.html", error="invalid credentials")
 
 
 @app.errorhandler(500)
@@ -16,4 +46,5 @@ def server_error(e):
 
 
 if __name__ == '__main__':
+    app.secret_key = "super secret key"
     app.run(debug=True, host="127.0.0.1", port=5000)
