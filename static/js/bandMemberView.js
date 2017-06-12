6b811d92aef1cd3768deb5eaf25411886ac454b9
Jake Gornall <jakegornall@yahoo.com>
2017-06-12 09:25:40 -0400

    fix: Change file input to only accept square images

--- static/js/bandMemberView.js
+++ static/js/bandMemberView.js
@@ -1,76 +1,124 @@
 var bandMemberView = bandMemberView || {};
 
 $(function() {
-	var BMView = Backbone.View.extend({
-		tagName : "div",
-		className : "band-member",
-		template : _.template($("#band-member-template").html()),
+    var BMView = Backbone.View.extend({
+        tagName : "div",
+        className : "band-member",
+        template : _.template($("#band-member-template").html()),
 
-		events : {
-			"click .remove" : "removeMember",
-			"click .edit-member" : "editMode",
-			"submit .band-member-edit-form" : "update"
-		},
+        events : {
+            "click .remove" : "removeMember",
+            "click .edit-member" : "editMode",
+            "submit .band-member-edit-form" : "update"
+        },
 
-		initialize: function() {
-    		this.listenTo(this.model, "change", this.render);
-    		this.listenTo(this.model, 'destroy', this.remove);
-  		},
+        initialize: function() {
+            this.listenTo(this.model, "change", this.render);
+            this.listenTo(this.model, 'destroy', this.remove);
+        },
 
-		render : function() {
-			this.$el.html(this.template(this.model.toJSON()));
-    		return this;
-		},
+        render : function() {
+            this.$el.html(this.template(this.model.toJSON()));
+            return this;
+        },
 
-		removeMember : function() {
-			this.model.destroy();
-		},
+        removeMember : function() {
+            this.model.destroy();
+        },
 
-		editMode : function() {
-			this.$el.find(".band-member-details").hide();
-			this.$el.find(".band-member-edit-form").show();
-		},
+        editMode : function() {
+            this.$el.find(".band-member-details").hide();
+            this.$el.find(".band-member-edit-form").show();
+        },
 
-		update : function(e) {
-    		e.preventDefault();
+        update : function(e) {
+            e.preventDefault();
 
-    		var that = this;
-    		var form = new FormData(this.$el.find(".band-member-edit-form")[0])
+            var that = this,
+                form = new FormData(this.$el.find(".band-member-edit-form")[0]),
+                fileInput = this.$el.find(".member-pic-input")[0],
+                file = fileInput.files && fileInput.files[0];
 
-    		$.ajax({
-    			type: "POST",
-    			url: "/bioPic",
-    			data: form,
-    			processData: false,
-    			contentType: false,
-    			success: function(resp) {
-    				if (resp.success) {
-						var newPicURL = resp.data;
-    				} else {
-    					var newPicURL = that.model.get("picURL")
-    				}
+            if( file ) {
+                var img = new Image();
+        
+                img.src = window.URL.createObjectURL( file );
+        
+                img.onload = function() {
+                    var width = img.naturalWidth,
+                        height = img.naturalHeight;
+        
+                    window.URL.revokeObjectURL( img.src );
+        
+                    if( width == 300 && height == 300 ) {
+                        $.ajax({
+                            type: "POST",
+                            url: "/bioPic",
+                            data: form,
+                            processData: false,
+                            contentType: false,
+                            success: function(resp) {
+                                if (resp.success) {
+                                    var newPicURL = resp.data;
+                                } else {
+                                    var newPicURL = that.model.get("picURL")
+                                }
 
-    				console.log(that.$el.find(".admin"));
+                                console.log(that.$el.find(".admin"));
 
-    				that.model.save({
-    					firstName: that.$el.find(".firstName").val(),
-    					lastName: that.$el.find(".lastName").val(),
-    					role: that.$el.find(".role").val(),
-    					email: that.$el.find(".email").val(),
-    					admin: that.$el.find(".admin")[0].checked,
-    					picURL: newPicURL
-    				});
-    				
-    				that.$el.find(".band-member-details").show();
-					that.$el.find(".band-member-edit-form").hide();
-    			},
-    			error: function() {
-    				console.log("error sending file");
-    			}
-    		});
-    	}
+                                that.model.save({
+                                    firstName: that.$el.find(".firstName").val(),
+                                    lastName: that.$el.find(".lastName").val(),
+                                    role: that.$el.find(".role").val(),
+                                    email: that.$el.find(".email").val(),
+                                    admin: that.$el.find(".admin")[0].checked,
+                                    picURL: newPicURL
+                                });
+                                
+                                that.$el.find(".band-member-details").show();
+                                that.$el.find(".band-member-edit-form").hide();
+                            },
+                            error: function() {
+                                console.log("error sending file");
+                            }
+                        });
+                    } else {
+                        that.$el.find(".error-msg").text("Image must be a square. Ex. w:300px h:300px");
+                    }
+                };
+            } else {
+                $.ajax({
+                    type: "POST",
+                    url: "/bioPic",
+                    data: form,
+                    processData: false,
+                    contentType: false,
+                    success: function(resp) {
+                        if (resp.success) {
+                            var newPicURL = resp.data;
+                        } else {
+                            var newPicURL = that.model.get("picURL")
+                        
+                            that.model.save({
+                                firstName: that.$el.find(".firstName").val(),
+                                lastName: that.$el.find(".lastName").val(),
+                                role: that.$el.find(".role").val(),
+                                email: that.$el.find(".email").val(),
+                                admin: that.$el.find(".admin")[0].checked,
+                                picURL: newPicURL
+                            });
+                            
+                            that.$el.find(".band-member-details").show();
+                            that.$el.find(".band-member-edit-form").hide();
+                        }
+                    },
+                    error: function() {
+                        console.log("error sending file");
+                    }
+                });
+            }
+        }
+    });
 
-	});
-
-	bandMemberView = BMView;
+    bandMemberView = BMView;
 });
